3f2fbc61dac2780b59f313015e93d2cef648742f
shubhangi rakhe <sr943@afsconnect2.njit.edu>
2017-11-20 03:35:37 -0500

    comments

--- index.php
+++ index.php
@@ -3,17 +3,17 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL);
 
 
-define('DATABASE', 'sr943');
+define('DATABASE', 'sr943');                   //connection parameters
 define('USERNAME', 'sr943');
 define('PASSWORD', 'SuVvKQn4');
 define('CONNECTION', 'sql1.njit.edu');
 
 $obj = new main();
 
-class globals
+class globals                       
 {
   
-  public static function all($html)
+  public static function all($html)              //static function for html tags
 
   {
     echo "$html"; 
@@ -34,7 +34,7 @@ class main {
     $html.= "<h3>Connected successfully </h3><hr>";    //conection message
     globals :: all($html);
 
-    $q= new All();
+    $q= new All();                                     //Class call for displaying all the collection and model functions
     $q-> query();
 
    
@@ -53,12 +53,12 @@ class All{
 
 public function query(){
 
-  $collection = new todos();
+    $collection = new todos();
     $resultset = $collection->fetch();
-    $line="<h3>All the records from todos table : </h3>";
-    table :: printtable($resultset,$line);
+    $line="<h3>All the records from todos table : </h3>";                   //function call for displaying all the records of todos table
+    table :: printtable($resultset,$line);                                  // static printtable function called for displaying table
     $id = 2;
-    $resultset = $collection->fetchOne($id);
+    $resultset = $collection->fetchOne($id);                               //function call for displaying one the records of todos table 
     //print_r(  $resultset )  ;
     $line="<h3>One record from todos table : </h3>";
     table :: printtable($resultset,$line);
@@ -66,17 +66,17 @@ public function query(){
 
     //$ac = new accounts();
     $collection1 = new accounts();
-    $resultset = $collection1->fetch();
+    $resultset = $collection1->fetch();                                    //function call for displaying all the records of accounts table
     $line="<h3>All the records from accounts table : </h3>";
     table :: printtable($resultset,$line);
     $id = 5;
-    $resultset = $collection1->fetchOne($id);
+    $resultset = $collection1->fetchOne($id);                              //function call for displaying one the records of accunts table
     $line="<h3>One record from accounts table : </h3>";
     table :: printtable($resultset,$line);
 
     $acObj = new account();
     $acObj->id = 12;
-    $acObj->save();
+    $acObj->save();                                                      // save function called from model class for update table
     
     $line="<h3>Phone number updated for id 12 from accounts table : </h3>";  
     $collection1 = new accounts();
@@ -87,7 +87,7 @@ public function query(){
 
 
     $td1 = new todo();
-    $td1->save();
+    $td1->save();                                                       // save function called from model class for insert table
     $collection = new todos();
     $resultset = $collection->fetch();
     $line="<h3>New row inserted in todos table with id as 901 : </h3>";
@@ -98,8 +98,7 @@ public function query(){
     $td = new todo();
     $collection = new todos();
     $id = 901;
-    $results = $td->delete($id);
-    //print_r($results);
+    $results = $td->delete($id);                                        //delete function called from model class
     $line="<h3>Record deleted from todos table where id is 901 : </h3>";    
     $resultset = $collection->fetch();         
     table :: printtable($resultset,$line);
@@ -142,7 +141,7 @@ class dbConn{
 
 
 class table{
-  public static function printtable($result,$line){
+  public static function printtable($result,$line){                          // class to display result into table format
 $html="";
       
       
@@ -173,12 +172,12 @@ $html="";
 class collection{
 
 
-      static public function create() {
+      static public function create() {                        //creating object of bean class
       $model = new static::$modelName;
       return $model;
     }
 
-  public function fetch(){
+  public function fetch(){                                 //This function displays all the records from the table
 
     
         $db = dbConn::getConnection();
@@ -194,7 +193,7 @@ class collection{
   
 
   
-  public function fetchOne($id){
+  public function fetchOne($id){                           //This function displays one record from the table
 
 
         $db = dbConn::getConnection();
@@ -227,7 +226,7 @@ class model {
     public function save()
     {
 
-        if ($this->id == '') {
+        if ($this->id == '') {                        // if existing id is not available it will insert otherwise it will update
             $sql = $this->insert();
         } else {
             $sql = $this->update();
@@ -243,11 +242,9 @@ class model {
 
        public function update(){
 
-        //$db = dbConn::getConnection();
+        
        $tableName = $this->tableName;         
-        $sql = "Update ". $tableName . " SET ".static::$column."='".static::$newphnNo."' WHERE id=" . $this->id;
-        //$statement = $db->prepare($sql);
-        //$statement->execute();             
+        $sql = "Update ". $tableName . " SET ".static::$column."='".static::$newphnNo."' WHERE id=" . $this->id;   //sql delete query         
         return $sql;
 
     }
@@ -256,7 +253,7 @@ class model {
 
         $db = dbConn::getConnection();
         $tableName = $this->tableName;        
-        $sql = "DELETE FROM " . $tableName .  " WHERE id = ". $id;
+        $sql = "DELETE FROM " . $tableName .  " WHERE id = ". $id;                                      //sql delete query
         $statement = $db->prepare($sql);
         $statement->execute();        
         return 'Deleted';
@@ -267,7 +264,7 @@ class model {
     public function insert(){
 
          
-        $sql = "INSERT INTO " . $this->tableName .  " VALUES ("  . static :: $string . ")";
+        $sql = "INSERT INTO " . $this->tableName .  " VALUES ("  . static :: $string . ")";                //sql insert query
         return $sql;
 
     }
@@ -275,8 +272,7 @@ class model {
 
 
 class account extends model {
-    public $id ;
-
+    public $id ;                               //bean class
     public $email ;
     public $fname ;
     public $lname ;
@@ -292,13 +288,13 @@ class account extends model {
     public function __construct(){
 
  
-        $this->tableName = 'accounts';
+        $this->tableName = 'accounts';       // setting table name as accounts
 
 }
 }
 
 class todo extends model {
-    public $id;
+    public $id;                              //bean class
     public $owneremail;
     public $ownerid;
     public $createddate;
@@ -312,7 +308,7 @@ class todo extends model {
     public function __construct(){
 
  
-        $this->tableName = 'todos';
+        $this->tableName = 'todos';           //setting tablename as todos
 
     }
 
